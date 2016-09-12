634a019eb2debd9016c83c5a03d2ef0c871b91d9
alexleshkevich <alexleshkevich@gmail.com>
2016-09-12 17:19:36 +0300

    add a initial Title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,28 +1,26 @@
 require 'spec_helper'
 
+
 describe "Static pages" do
 
   describe "Home page" do
 
-    it "should have the right title" do
-  visit '/static_pages/home'
-  expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
-end
-  end
+    let(:base_title) { "Ruby on Rails Tutorial Sample App" }
 
-  describe "Help page" do
+    it "should have the content 'Sample App'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('Sample App')
+    end
 
-    it "should have the right title" do
-      visit '/static_pages/help'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+    it "should have the base title" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App")
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title('| Home')
     end
   end
 
-  describe "About page" do
-
-	  it "should have the right title" do
-	    visit '/static_pages/about'
-	    expect(page).to have_title("Ruby on Rails Tutorial Sample App | About us")
-	  end
-	end
 end
